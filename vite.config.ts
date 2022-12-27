7e0fbdb98562fe9682f7c9d57e43fe56c8967dcf
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2022-12-27 11:24:52 +0200

    dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
-
+import dts from 'vite-plugin-dts'
 import path from 'path'
 
 export default defineConfig({
@@ -23,5 +23,5 @@ export default defineConfig({
         },
     },
 
-    plugins: [vue(), eslintPlugin()],
+    plugins: [vue(), eslintPlugin(), dts()],
 })
