eebae6210435497092cf402a4d57e9f3f2d3a072
OleksandrYastrebov <frontwork0405@gmail.com>
2023-01-12 21:09:32 +0200

    tabs

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,62 @@ import VAccordion from './components/Accordion/VAccordion.vue'
 import VPagination from './components/Pagination/VPagination.vue'
 import VTimeline from './components/Timeline/VTimeline.vue'
 import VMenu from './components/Menu/VMenu.vue'
+import VTabs from './components/Tabs/VTabs.vue'
 import { UserIcon } from '@jonyak/icons/24/outline'
-import { MenuItem } from '@headlessui/vue';
+import { MenuItem, TabPanel, Tab } from '@headlessui/vue';
 
 const test = ['20.08', '30.05', '40.05', '59.81', '04,03']
+
+const categories = ref({
+  Recent: [
+    {
+      id: 1,
+      title: 'Does drinking coffee make you smarter?',
+      date: '5h ago',
+      commentCount: 5,
+      shareCount: 2,
+    },
+    {
+      id: 2,
+      title: "So you've bought coffee... now what?",
+      date: '2h ago',
+      commentCount: 3,
+      shareCount: 2,
+    },
+  ],
+  Popular: [
+    {
+      id: 1,
+      title: 'Is tech making coffee better or worse?',
+      date: 'Jan 7',
+      commentCount: 29,
+      shareCount: 16,
+    },
+    {
+      id: 2,
+      title: 'The most innovative things happening in coffee',
+      date: 'Mar 19',
+      commentCount: 24,
+      shareCount: 12,
+    },
+  ],
+  Trending: [
+    {
+      id: 1,
+      title: 'Ask Me Anything: 10 answers to your questions about coffee',
+      date: '2d ago',
+      commentCount: 9,
+      shareCount: 5,
+    },
+    {
+      id: 2,
+      title: "The worst advice we've ever heard about coffee",
+      date: '4d ago',
+      commentCount: 1,
+      shareCount: 2,
+    },
+  ],
+})
 
 const selected = ref('')
 </script>
@@ -25,6 +77,74 @@ const selected = ref('')
       <UserIcon class="btn-left-icon" />
       Click
     </v-button>
+  </div>
+
+  <div class="m-10 flex items-center justify-center space-x-2 p-10 shadow-2xl">
+    <v-tabs>
+      <template #default>
+        <Tab
+          v-for="category in Object.keys(categories)"
+          :key="category"
+          v-slot="{ selected }"
+          as="template"
+        >
+          <button
+            :class="[
+              'w-full outline-none py-2.5 text-base  font-medium leading-5  border-b-2 border-transparent',
+              selected
+                ? 'border-b-primary-500'
+                : 'text-blue-100 ',
+            ]"
+          >
+            {{ category }}
+            <span
+            class="tag tag-primary"
+            :class="{'text-white bg-primary-500' : selected}"
+            >Label</span>
+          </button>
+        </Tab>
+      </template>
+      <template #panels>
+        <TabPanel
+          v-for="(posts, idx) in Object.values(categories)"
+          :key="idx"
+          :class="[
+            'rounded-xl bg-white p-3',
+            'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+          ]"
+        >
+          <ul>
+            <li
+              v-for="post in posts"
+              :key="post.id"
+              class="relative rounded-md p-3 hover:bg-gray-100"
+            >
+              <h3 class="text-sm font-medium leading-5">
+                {{ post.title }}
+              </h3>
+
+              <ul
+                class="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500"
+              >
+                <li>{{ post.date }}</li>
+                <li>&middot;</li>
+                <li>{{ post.commentCount }} comments</li>
+                <li>&middot;</li>
+                <li>{{ post.shareCount }} shares</li>
+              </ul>
+
+              <a
+                href="#"
+                :class="[
+                  'absolute inset-0 rounded-md',
+                  'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2',
+                ]"
+              />
+            </li>
+          </ul>
+        </TabPanel>
+      </template>
+    </v-tabs>
   </div>
 
   <div class="my-10 mx-auto p-10 shadow-2xl">
---
+++ src/components/Tabs/VTabs.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full max-w-md px-2 py-16 sm:px-0">
+    <TabGroup>
+      <TabList class="flex space-x-1  p-1">
+        <slot />
+      </TabList>
+      <TabPanels>
+        <slot name="panels"></slot>
+      </TabPanels>
+
+
+    </TabGroup>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { TabGroup, TabList, TabPanels  } from '@headlessui/vue'
+
+
+
+</script>
