f04cb580538d2b7f3848d3ade21c6d6009ac2e68
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2023-02-08 17:37:10 +0200

    export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,14 @@
 export { default as VButton } from './Button/VButton.vue'
+export { default as VInput } from './Input/VInput.vue'
+export { default as VAccordion } from './Accordion/VAccordion.vue'
+export { default as VAlert } from './Alert/VAlert.vue'
 export { default as VAvatar } from './Avatar/VAvatar.vue'
+export { default as VAvatarGroup } from './AvatarGroup/VAvatarGroup.vue'
+export { default as VBadge } from './Badge/VBadge.vue'
+export { default as VMenu } from './Menu/VMenu.vue'
+export { default as VNotification } from './Notification/VNotification.vue'
+export { default as VPagination } from './Pagination/VPagination.vue'
+export { default as VSwitch } from './Switch/VSwitch.vue'
+export { default as VTabs } from './Tabs/VTabs.vue'
 
 // Other components you plan to publish go here
