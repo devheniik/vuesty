148d2fab9d436c4639ad72235d9d3d21cf6da6c5
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2023-02-09 16:30:02 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-import { defineConfig, type PluginOption } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint'
-import { visualizer } from 'rollup-plugin-visualizer'
 import type { UserConfig as VitestUserConfigInterface } from "vitest/config"
 import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
@@ -16,7 +14,6 @@ const vitestConfig: VitestUserConfigInterface = {
 export default defineConfig({
   build: {
     target: ['edge90', 'chrome90', 'firefox90', 'safari15'],
-    outDir: './dist',
     lib: {
       formats: ['es', 'cjs'],
       entry: resolve(__dirname, './src/index.ts'),
@@ -24,19 +21,25 @@ export default defineConfig({
       fileName: 'index',
     },
     rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
       external: ['vue'],
       output: {
         exports: 'named',
-        globals: { vue: 'Vue' }
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        },
       },
     },
   },
   test: vitestConfig.test,
-  plugins: [vue(), eslintPlugin(),
+  plugins: [vue(),
     dts({
       beforeWriteFile: (filePath, content) => {
         const newFilePath = filePath
-          .replace('/src', '/types');
+          .replace('./', './types');
 
         return {
           filePath: newFilePath,
@@ -44,9 +47,6 @@ export default defineConfig({
         };
       },
       skipDiagnostics: true,
-    }),
-    visualizer({
-      emitFile: true
-    }) as PluginOption
+    })
   ],
 })
