9dfb8e1b8b3ca62da363e19eac974a8439d46531
Oleksandr <54812745+OleksandrYastrebov@users.noreply.github.com>
2023-03-09 17:41:46 +0200

    Update VUTable.vue

--- src/components/UTable/VUTable.vue
+++ src/components/UTable/VUTable.vue
@@ -51,13 +51,11 @@ const props = withDefaults(
     rows: string[] | object[] | number[]
     headers: string[],
     cols?: string[] | object[] | number[] | null
-    deleteAction?: boolean,
     editAction?: boolean,
     menuOptions: string[] | string,
   }>(),
   {
     cols: null,
-    deleteAction: true,
     editAction: true,
   }
 )
