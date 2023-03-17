4d3c8e3f282b7cb9756549746a44415480d2af0e
Oleksandr <54812745+OleksandrYastrebov@users.noreply.github.com>
2023-03-17 13:48:18 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    colors: {
+        colors: {
       transparent: 'transparent',
       primary: {
         50: '#EDF6FF',
