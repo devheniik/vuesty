eda20ec38029e4caee9e14f4338ffd3784ba669a
Oleksandr <alexhawkhawkalex@gmail.com>
2024-08-01 18:05:49 +0300

    fix: VSelect cant deselect on disabled state

--- src/components/Select/VSelect.vue
+++ src/components/Select/VSelect.vue
@@ -393,7 +393,7 @@ onMounted(() => {
           <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
       </div>
-      <div v-else class="v-select__icon-box">
+      <div v-else-if="!disabled" class="v-select__icon-box">
         <CloseMdIcon
           v-if="(modelValue !== null && modelValue !== undefined) && deselect"
           :class="[ 'v-select__icon focus-none', { 'v-select__icon_focus' : isFocused }]"
